73e52bc2794fea1aa2c38f267692e3c73c14b414
John Agbo <Jonchukwuma04@gmail.com>
2022-07-14 06:46:35 -0700

    This code takes no action

---
+++ op_nop.c
@@ -0,0 +1,16 @@
+#include "monty.h"
+
+/**
+ * nop - This function takes no action
+ *
+ * @stack: The head of the stack
+ * @line_number: The line on which the error occurred
+ *
+ * Return: Nothing
+ */
+
+void nop(stack_t **stack, unsigned int line_number)
+{
+	(void) stack;
+	(void) line_number;
+}
